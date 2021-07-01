8958bd45e4ea0f5f7859fa023b2c70b8ddbbfe87
Wouter <wouter.vanderheijde@wearetriple.com>
2021-07-02 01:44:03 +0200

    :recycle: Refactor existing components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="root">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
+<script lang="ts">
+import Vue from "vue";
 import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
+export default Vue.extend({
   name: "Home",
   components: {
-    HelloWorld
-  }
-};
+    HelloWorld,
+  },
+});
 </script>
