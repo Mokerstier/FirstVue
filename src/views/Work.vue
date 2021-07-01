ba070a724007fbaed890948955dab9ab6affba24
Wouter <wouter.vanderheijde@wearetriple.com>
2021-07-02 01:42:08 +0200

    :sparkles: View showcase work

---
+++ src/views/Work.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="root">
+    <section class="basic-grid" v-if="posts.length">
+      <h2>Showcase</h2>
+      <div v-for="post in posts" :key="post.id">
+        <Article v-bind:post="post" />
+      </div>
+    </section>
+    <div v-else>
+      <p class="no-results">There are currently no posts</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Article from "@/components/Article.vue";
+import { mapState } from "vuex";
+
+export default {
+  name: "Work",
+  data() {
+    return {
+      selectedPost: {},
+      showPostModal: false,
+      fullPost: {},
+    };
+  },
+  components: {
+    Article,
+  },
+  computed: {
+    ...mapState(["posts"]),
+  },
+  methods: {},
+};
+</script>
